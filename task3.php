3d4181199dcc5da0d1244cccab3112b76af74828
iftekharabir <iftekhar.abir1@gmail.com>
2023-12-06 17:29:42 +0600

    Done

---
+++ task3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Module 1</title>
+</head>
+<body>
+
+
+<?php
+//PHP Function
+    
+    $firstName = "Iftekhar";
+    $lastName = "Uddin";
+    $nickName = "Abir";
+    $age = "29";
+    $gender = "Male";
+
+/*PHP Output
+echo "$firstName<br> $lastName<br> $nickName<br> $age<br> $gender";
+?>
+*/
+</body>
+</html>
